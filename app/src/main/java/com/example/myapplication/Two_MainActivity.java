f2965685b4289d5d3a45827779b61964e7eb466b
fenwuwen <fenwuwen@gmail.com>
2021-10-05 07:33:00 +0800

    add LinearLayout.

--- app/src/main/java/com/example/myapplication/Two_MainActivity.java
+++ app/src/main/java/com/example/myapplication/Two_MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextClock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +10,10 @@ public class Two_MainActivity  extends AppCompatActivity {
     @Override
     public void onCreate(Bundle a) {
         super.onCreate(a);
+        LinearLayout linearLayout=new LinearLayout(this);
+        TextClock textClock=new TextClock(this);
+        linearLayout.addView(textClock);
+        setContentView(linearLayout);
 
 
     }
